b0e5d698e1dcc08b9328651112e359383d37f3fc
schwarzfelix <fschwarz2000@hotmail.de>
2025-04-19 18:41:30 +0200

    chore: change default displayconfig

--- presentation/displayconfig.py
+++ presentation/displayconfig.py
@@ -1,11 +1,11 @@
 class DisplayConfig():
     def __init__(self):
 
-        self.showAnchorCircles = False
-        self.showAnchorLabels = False
+        self.showAnchorCircles = True
+        self.showAnchorLabels = True
 
-        self.showTagAnchorLabels = False
-        self.showTagAnchorLines = False
+        self.showTagAnchorLabels = True
+        self.showTagAnchorLines = True
 
         self.showBetweenAnchorsLabels = False
-        self.showBetweenAnchorsLines = False
+        self.showBetweenAnchorsLines = True
