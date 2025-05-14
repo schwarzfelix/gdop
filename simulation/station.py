0149ab3a8ef44cb168fa0b1f0ad93da54d373379
schwarzfelix <fschwarz2000@hotmail.de>
2025-05-14 15:19:41 +0200

    feature: add __repr__ method to Station class for improved string representation

--- simulation/station.py
+++ simulation/station.py
@@ -38,6 +38,9 @@ class Station(ABC):
     def __str__(self):
         return self.name()
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name()})"
+
 class Anchor(Station):
 
     def __init__(self, position, name='FixedDevice', scenario=None):
